ee4d747da80f5b59f557a4e0d8e0794239c5d18c
WIjaksara Aptaluhung <19623024@std.stei.itb.ac.id>
2026-05-05 18:13:26 +0700

    feat: haduh

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 3000;
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`AquaChain API running on port ${PORT}`);
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`🐟 AquaChain API running on port ${PORT}`);
-});
+module.exports = app;
